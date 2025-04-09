52e4433ed976870fb6b77fe373c6e92e801e7396
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-04-09 10:58:23 -0300

    adding update and delete logic for service layer

--- src/main/java/com/example/dudu/ContactList/service/ContactService.java
+++ src/main/java/com/example/dudu/ContactList/service/ContactService.java
@@ -2,12 +2,15 @@ package com.example.dudu.ContactList.service;
 
 import com.example.dudu.ContactList.dto.ContactDTO;
 import com.example.dudu.ContactList.dto.CreateContactRequest;
+import com.example.dudu.ContactList.dto.UpdateContactRequest;
 import com.example.dudu.ContactList.entities.Contact;
 import com.example.dudu.ContactList.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ContactService {
 
     @Autowired
@@ -27,7 +30,17 @@ public class ContactService {
         return contacts;
     }
 
+    public Contact updateContact(Long id, UpdateContactRequest request) {
+        Contact contact = repository.findById(id).orElseThrow(() -> new RuntimeException(
+                "Contact not found with id: " + id)
+        );
+        contact.setName(request.getName());
+        contact.setLastName(request.getLastName());
+        contact.setNumber(request.getNumber());
+        return repository.save(contact);
+    }
 
-
-
+    public void deleteContact(Long id) {
+        repository.deleteById(id);
+    }
 }
