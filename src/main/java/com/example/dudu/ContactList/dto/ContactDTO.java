6feef1af0dfb7e3202f509fb425c7ac8655e556b
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-04-10 09:36:01 -0300

    Moving DTOs package for better organization

--- src/main/java/com/example/dudu/ContactList/dto/ContactDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.dudu.ContactList.dto;
-
-public record ContactDTO(String name, String lastName, String number) {
-
-    public ContactDTO(com.example.dudu.ContactList.entities.Contact data) {
-        this(data.getName(), data.getLastName(), data.getNumber());
-    }
-}
