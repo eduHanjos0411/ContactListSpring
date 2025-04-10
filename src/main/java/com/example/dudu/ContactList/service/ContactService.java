147984e9e276cdf8276deba4b87d1ba0d16ed1b1
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-04-10 09:35:20 -0300

    Refactoring ContactService to match new response format

--- src/main/java/com/example/dudu/ContactList/service/ContactService.java
+++ src/main/java/com/example/dudu/ContactList/service/ContactService.java
@@ -1,8 +1,7 @@
 package com.example.dudu.ContactList.service;
 
-import com.example.dudu.ContactList.dto.ContactDTO;
-import com.example.dudu.ContactList.dto.CreateContactRequest;
-import com.example.dudu.ContactList.dto.UpdateContactRequest;
+import com.example.dudu.ContactList.dto.ContactResponseDTO;
+import com.example.dudu.ContactList.dto.request.ContactRequestDTO;
 import com.example.dudu.ContactList.entities.Contact;
 import com.example.dudu.ContactList.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +15,7 @@ public class ContactService {
     @Autowired
     public ContactRepository repository;
 
-    public Contact addNewContact(CreateContactRequest request) {
+    public Contact addNewContact(ContactRequestDTO request) {
         Contact ctt = new Contact();
 
         ctt.setName(request.getName());
@@ -25,12 +24,12 @@ public class ContactService {
         return repository.save(ctt);
     }
 
-    public List<ContactDTO> getAllcontacts() {
-        List<ContactDTO> contacts = repository.findAll().stream().map(ContactDTO::new).toList();
+    public List<ContactResponseDTO> getAllcontacts() {
+        List<ContactResponseDTO> contacts = repository.findAll().stream().map(ContactResponseDTO::new).toList();
         return contacts;
     }
 
-    public Contact updateContact(Long id, UpdateContactRequest request) {
+    public Contact updateContact(Long id, ContactRequestDTO request) {
         Contact contact = repository.findById(id).orElseThrow(() -> new RuntimeException(
                 "Contact not found with id: " + id)
         );
