86ead8daa0f8d9f75c71588f0598b7e2d7d2fa8e
Eduardo Henrique <eduardo.h.anjos0411@gmail.com>
2025-04-10 09:32:37 -0300

    Adding standard format to the endpoint response

--- src/main/java/com/example/dudu/ContactList/controllers/ContactController.java
+++ src/main/java/com/example/dudu/ContactList/controllers/ContactController.java
@@ -1,8 +1,7 @@
 package com.example.dudu.ContactList.controllers;
 
-import com.example.dudu.ContactList.dto.ContactDTO;
-import com.example.dudu.ContactList.dto.CreateContactRequest;
-import com.example.dudu.ContactList.dto.UpdateContactRequest;
+import com.example.dudu.ContactList.dto.ContactResponseDTO;
+import com.example.dudu.ContactList.dto.request.ContactRequestDTO;
 import com.example.dudu.ContactList.entities.Contact;
 import com.example.dudu.ContactList.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +18,21 @@ public class ContactController {
     @Autowired
     public ContactService service;
 
-    @PostMapping("/newContact")
-    public ResponseEntity<Contact> addNewContact(@RequestBody CreateContactRequest ctt) {
+    @PostMapping
+    public ResponseEntity<Contact> addNewContact(@RequestBody ContactRequestDTO ctt) {
        Contact newCtt = service.addNewContact(ctt);
         URI location = URI.create("/newContact" + newCtt.getId());
        return ResponseEntity.created(location).body(newCtt);
     }
 
     @GetMapping("/showAllContacts")
-    public ResponseEntity<List<ContactDTO>> showAllContacts() {
-        List<ContactDTO> allCtts = service.getAllcontacts();
+    public ResponseEntity<List<ContactResponseDTO>> showAllContacts() {
+        List<ContactResponseDTO> allCtts = service.getAllcontacts();
         return ResponseEntity.ok(allCtts);
     }
 
     @PutMapping("/updtContact/{id}")
-    public ResponseEntity<Contact> updtContact(@PathVariable Long id, @RequestBody UpdateContactRequest data) {
+    public ResponseEntity<Contact> updtContact(@PathVariable Long id, @RequestBody ContactRequestDTO data) {
         Contact updtCtt = service.updateContact(id, data);
         return ResponseEntity.ok(updtCtt);
     }
